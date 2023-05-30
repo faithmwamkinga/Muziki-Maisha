a4a6a59eb67321377da17f788f4e81e4eb437aa1
faithmwamkinga <faithmwamkinga@gmail.com>
2023-05-30 07:45:14 +0300

    adding tours and events to the website

---
+++ script.js
@@ -0,0 +1,32 @@
+// {function revealOnScroll() {
+//     const fiesta = document.querySelector('.fiesta');
+//     const windowHeight = window.innerHeight;
+//     const scrollY = window.scrollY || window.pageYOffset;
+//     const containerTop = fiesta.getBoundingClientRect().top + scrollY;
+  
+//     if (scrollY + windowHeight > containerTop) {
+//       fiesta.classList.add('reveal');
+//     }
+//   }
+  
+//   window.addEventListener('scroll', revealOnScroll);
+  
+    // /* <script type="text/javascript">
+    //     window.addEventListener("scroll",reveal);
+    //     function reveal(){
+    //         var reveals =document.querySelectorAll(`.reveal`)
+    //         for (var i =0; i=reveal.length;i++){
+    //             var windowheight=window.innerHeight;
+    //             var revealtop=reveals[i].getBoundingClientRect().top
+    //             var revealpoint=150;
+
+    //             if (revealtop<windowheight-revealpoint){
+    //                 reveals[i].classList.add('active')
+    //             }
+    //             else{
+    //                 reveals[i].classList.remove('active')
+    //             }
+                
+    //         }
+    //     }
+    // </script> */}
